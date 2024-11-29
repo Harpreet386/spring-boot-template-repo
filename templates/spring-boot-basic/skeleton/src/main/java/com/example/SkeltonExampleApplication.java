ceb4f55d782d94bd56ab44c99a1e1a6a8517c02d
EAD\hsingh296 <er.harpreetsingh28@gmail.com>
2024-11-29 19:12:51 +0530

    Initial Spring Boot API

---
+++ templates/spring-boot-basic/skeleton/src/main/java/com/example/SkeltonExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkeltonExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkeltonExampleApplication.class, args);
+	}
+
+}
